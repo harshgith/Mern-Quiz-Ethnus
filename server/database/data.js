55f85c58257bca5e20ab7f3916c007aa2e7c08e4
harshgith <96532749+harshgith@users.noreply.github.com>
2023-11-23 22:59:50 +0530

    Add files via upload

---
+++ server/database/data.js
@@ -0,0 +1,49 @@
+export default [
+    {
+        id: 1,
+        question : "Which of the following is used in React.js to increase performance? ",
+        options : [
+            'Virtual DOM',
+            'Orignal DOM',
+            'Both A and B',
+        ]
+    },
+    {
+        id: 2,
+        question : "Following methods can be used to display data in some form using Javascript",
+        options : [
+            'document.write()',
+            'console.log()',
+            'window.alert()',
+        ]
+    },
+    {
+        id: 3,
+        question : "When an operator value is NULL, the typeof returned by the unary operator is:",
+        options : [
+            'Boolean',
+            'Undefined',
+            'Object',
+        ]
+    },
+    {
+        id: 4,
+        question : "What does the toString() method return?",
+        options : [
+            'Return Object',
+            'Return String',
+            'Return Integer'
+        ]
+    },
+    {
+        id: 5,
+        question : "Identify the one which is used to pass data to components from outside?",
+        options : [
+            'props',
+            'propTypes',
+            'setState',
+        ]
+    }
+];
+
+export const answers = [0, 1, 2, 1, 0];
